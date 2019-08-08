98d9332b96916fcba5faf391f5dd0a74e9234345
Dmitry Shibanov <shibanov-1997@inbox.ru>
2019-08-08 01:24:16 -0700

    Create appcenter-pre-build.sh

---
+++ XWeather/appcenter-pre-build.sh
@@ -0,0 +1,4 @@
+pushd /Library/Frameworks/Xamarin.Android.framework/Versions/9.4.0.51/lib
+sudo ln -sf xamarin.android/xbuild xbuild
+sudo ln -sf xamarin.android/xbuild-frameworks xbuild-frameworks
+popd
